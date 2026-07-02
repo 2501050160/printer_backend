4f7f8904a7d95a732ee45270a5162801130be2e6
DHANUSHKUMMARI <dhanushkummari781@gmail.com>
2026-07-02 14:54:22 +0530

    Add seedAdmin method to create default admin account if table is empty

--- src/main/java/com/saipraveen/login_registration/LoginRegistrationApplication.java
+++ src/main/java/com/saipraveen/login_registration/LoginRegistrationApplication.java
@@ -12,4 +12,17 @@ public class LoginRegistrationApplication {
 		SpringApplication.run(LoginRegistrationApplication.class, args);
 	}
 
+	@org.springframework.context.annotation.Bean
+	public org.springframework.boot.CommandLineRunner seedAdmin(com.saipraveen.login_registration.repository.AdminRepository adminRepository) {
+		return args -> {
+			if (adminRepository.count() == 0) {
+				com.saipraveen.login_registration.entity.Admin admin = new com.saipraveen.login_registration.entity.Admin();
+				admin.setUsername("admin");
+				admin.setPassword("admin");
+				adminRepository.save(admin);
+				System.out.println("Default admin user created: admin / admin");
+			}
+		};
+	}
+
 }
